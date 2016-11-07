b8c6b6b73c8953baaaf258fcdd56df160629a61e
CCS <ccs6327@gmail.com>
2016-11-08 00:46:41 +0800

    use warp perspective to transform images

--- project.py
+++ project.py
@@ -75,11 +75,11 @@ def homography_mapping(video_image, first_frame, homography_matrix):
     for h in range(height):
         for w in range(width):
             # convert pixel to 3-D point by appending depth as 1
-            point = np.append([h, w], [1])
+            point = np.append([w, h], [1])
             new_pos_x, new_pos_y, new_pos_z = np.dot(homography_matrix, point)
             new_pos_x, new_pos_y = int(new_pos_x), int(new_pos_y)
             try:
-                new_video_image[new_pos_x][new_pos_y] = video_image[h][w]
+                new_video_image[new_pos_y][new_pos_x] = video_image[h][w]
             except IndexError:
                 continue
     return new_video_image
@@ -101,11 +101,11 @@ def inverse_homography_mapping(video_image, first_frame, inverse_homography_matr
     for h in range(height):
         for w in range(width):
             # convert pixel to 3-D point by appending depth as 1
-            point = np.append([h, w], [1])
+            point = np.append([w, h], [1])
             new_pos_x, new_pos_y, new_pos_z = np.dot(inverse_homography_matrix, point)
             new_pos_x, new_pos_y = int(new_pos_x), int(new_pos_y)
             try:
-                new_video_image[h][w] = video_image[new_pos_x][new_pos_y]
+                new_video_image[h][w] = video_image[new_pos_y][new_pos_x]
             except IndexError:
                 continue
     return new_video_image
@@ -161,28 +161,34 @@ def main():
         marked_images = []
         estimated_pixels = []
         all_selected_pixels = []
-        skip_frame = 20
-        for start_index in range(0, len(video_images), skip_frame):
-            cv2.destroyAllWindows()
-            if start_index > 0:
-                cv2.imshow(str(start_index - skip_frame), marked_images[-skip_frame])
-            start_frame = video_images[start_index]
-            selected_pixels = handpickPixel.handpick_image(start_frame, estimated_pixels)
-            all_selected_pixels.append(selected_pixels)
-            temp_marked_images, marked_frame_coordinates, status_arr = changeDetection.mark_features_on_all_images(video_images[start_index: start_index + skip_frame + 1], selected_pixels)
-            estimated_pixels = getLastCoordinatesWithStatusArr(marked_frame_coordinates, status_arr)
-            marked_images = marked_images + temp_marked_images
-        cv2.destroyAllWindows()
-
-        util.save_coordinates(video_file_name, all_selected_pixels)
-        # all_selected_pixels = util.load_coordinates(video_file_name)
+        skip_frame = 10
+        # for start_index in range(0, len(video_images), skip_frame):
+        #     cv2.destroyAllWindows()
+        #     if start_index > 0:
+        #         cv2.imshow(str(start_index - skip_frame), marked_images[-skip_frame])
+        #     start_frame = video_images[start_index]
+        #     selected_pixels = handpickPixel.handpick_image(start_frame, estimated_pixels)
+        #     all_selected_pixels.append(selected_pixels)
+        #     temp_marked_images, marked_frame_coordinates, status_arr = changeDetection.mark_features_on_all_images(video_images[start_index: start_index + skip_frame + 1], selected_pixels)
+        #     estimated_pixels = getLastCoordinatesWithStatusArr(marked_frame_coordinates, status_arr)
+        #     marked_images = marked_images + temp_marked_images
+        # cv2.destroyAllWindows()
+        #
+        # util.save_coordinates(video_file_name, all_selected_pixels)
+        all_selected_pixels = util.load_coordinates(video_file_name)
 
         homography_matrixes = []
         # skip the first frame
         for selected_pixel in all_selected_pixels[1:]:
             # H = homography.find_homography(marked_frame_coordinates[0], mark_frame_coordinate)
-            H, inliers = cv2.findHomography(np.float32(all_selected_pixels[0]), np.float32(selected_pixel), cv.CV_RANSAC)
+            H, inliers = cv2.findHomography(np.float32(selected_pixel), np.float32(all_selected_pixels[0]), cv.CV_RANSAC)
             homography_matrixes.append(H)
+
+        new_video_images = []
+        new_video_images.append(first_frame)
+        for index, matrix in enumerate(homography_matrixes):
+            result = cv2.warpPerspective(video_images[(index + 1) * skip_frame], matrix, (width, height))
+            new_video_images.append(result)
 
         inverse_homography_matrixes = []
         # calculate the homography inverse
@@ -190,13 +196,11 @@ def main():
             inverse_homography_matrix = la.inv(homography_matrix)
             inverse_homography_matrixes.append(inverse_homography_matrix)
 
-        new_video_images = []
-        new_video_images.append(first_frame)
 
         # paralleling the homography mapping
         num_cores = multiprocessing.cpu_count()
-        new_video_images = Parallel(n_jobs=num_cores, verbose=11)(delayed(homography_mapping)(video_images[(i+1) * skip_frame], first_frame, homography_matrixes[i]) for i in range(len(homography_matrixes)))
-        # new_video_images = Parallel(n_jobs=num_cores, verbose=11)(delayed(inverse_homography_mapping)(video_images[(i+1) * skip_frame], first_frame, inverse_homography_matrixes[i], i) for i in range(len(inverse_homography_matrixes)))
+        # new_video_images = Parallel(n_jobs=num_cores, verbose=11)(delayed(homography_mapping)(video_images[(i+1) * skip_frame], first_frame, homography_matrixes[i]) for i in range(len(homography_matrixes)))
+        # new_video_images = Parallel(n_jobs=num_cores, verbose=11)(delayed(inverse_homography_mapping)(video_images[(i+1) * skip_frame], first_frame, inverse_homography_matrixes[i]) for i in range(len(inverse_homography_matrixes)))
 
         print 'Done with homography calculation. Writing to file now...'
         video_path = os.path.join(video_file_name, video_file_name + '_homography_orig')
